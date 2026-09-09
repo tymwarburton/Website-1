6b2cad7d05d602b759e516866ca05b64129cd99b
Ty <119599808+Tyisvibing@users.noreply.github.com>
2026-09-09 23:20:50 +0100

    updated popPicture to work on all images

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,12 +1,13 @@
-function popPicture(){
-    picture = document.querySelector("#Image")
-    
 
-    picture.classList.add("pop");
-    setTimeout(function(){
-        picture.classList.remove("pop");
+function popPicture(imageElement) {
+    imageElement.classList.add("pop");
+    setTimeout(function () {
+        imageElement.classList.remove("pop");
     }, 300);
-
 }
 
-document.querySelector("#Image").addEventListener("click",popPicture)
+document.querySelectorAll("img").forEach(function (image) {
+    image.addEventListener("click", function () {
+        popPicture(image);
+    });
+});
