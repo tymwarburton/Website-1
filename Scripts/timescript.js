d116dafec142f8ddfef5983f536525e0ed3150ee
Ty <119599808+Tyisvibing@users.noreply.github.com>
2026-09-11 17:19:30 +0100

    idk

--- Scripts/timescript.js
+++
@@ -1,9 +0,0 @@
-//get current time and date
-//get lesson 1 times
-//is lesson 1 in progress
-//if lesson 1 is in progress displa a progress bar and the time left in the lesson
-//display next lesson and the time until it starts and the time it starts at
-//on the right/left side of the page display a list of all lessons for the day
-//check for lesson updates every minute and update the page accordingly
-//if time till next lesson is 0 display lesson 2 and perform the same checks for lesson 2
-//if lesson number is = to the total number of lessons for the day change time till next lesson to time till end of day and do not check for next lesson
