705c48510775d1b2c435b063e7392cf727e55b85
Ty <119599808+Tyisvibing@users.noreply.github.com>
2026-09-13 00:33:51 +0100

    added more text

--- Scripts/script.js
+++ Scripts/script.js
@@ -14,7 +14,7 @@ document.querySelectorAll("img").forEach(function (image) {
 
 function aboutButtonClicked() {
     const currentTime = new Date().toLocaleTimeString();
-    document.getElementById("about-button").textContent = currentTime;
+    document.getElementById("about-button").textContent = "Current Time is: " + currentTime;
 }
 
 
