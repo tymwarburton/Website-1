5e2d239e64271f3a70e465ca9d0c71c54ed71a40
Ty <119599808+Tyisvibing@users.noreply.github.com>
2026-09-13 00:26:45 +0100

    added time to about button

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,4 +1,4 @@
-
+const currentTime = new Date().toLocaleString();
 function popPicture(imageElement) {
     imageElement.classList.add("pop");
     setTimeout(function () {
@@ -13,7 +13,8 @@ document.querySelectorAll("img").forEach(function (image) {
 });
 
 function aboutButtonClicked() {
-    document.getElementById("about-button").textContent = "Clicked!";
+    document.getElementById("about-button").textContent = currentTime;
 }
+
 
 document.getElementById("about-button").addEventListener("click", aboutButtonClicked);
